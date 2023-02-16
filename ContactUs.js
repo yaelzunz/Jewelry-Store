fd04ce0075bb301d18866f3eb2b195d3768d9e7c
yaelzunz <117014260+yaelzunz@users.noreply.github.com>
2023-02-16 20:13:34 +0200

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ContactUs() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <br></br>
+
+      <h4>Yael: 0740254057
+        <br></br>
+        Keren: 0758965423
+      </h4>
+    </div>
+  )
+}
