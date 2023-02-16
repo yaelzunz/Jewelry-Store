d85d99b372278b663b158a6876abc77b9d58ce0a
yaelzunz <117014260+yaelzunz@users.noreply.github.com>
2023-02-16 20:17:07 +0200

    Add files via upload

--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div>
+      <h2>Contact Us</h2>
+      <br></br>
+
+      <h4>Yael: 0740254057
+        <br></br>
+        Keren: 0758965423
+      </h4>
+    </div>
   )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className='container'>
       <h1 className='text-center'>Welcome to our jewelry store</h1>
-      <img height={500} width={500} src="https://i.pinimg.com/564x/1d/fa/37/1dfa37d940ed4c21ac04f09869c611c4.jpg" alt='img'/>
+      <img height={500} width={400} src="https://i.pinimg.com/564x/1b/69/03/1b6903976ff9de2394109865f9afaf11.jpg" alt='img'/>
       <br></br><br></br>
       <h3 className='text-center'>Our jewelry</h3>
       <button className='btn btn-dark m-2'>
